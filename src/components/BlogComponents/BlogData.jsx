b7086b452fc96be26c3796b1a1540c237c109196
Admolgit <zealous890@gmail.com>
2022-12-13 23:48:56 +0100

    blog

---
+++ src/components/BlogComponents/BlogData.jsx
@@ -0,0 +1,64 @@
+import BlogImg from "../../assets/images/blogimg.png";
+
+export const BlogData = [
+  {
+    id: 1,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 2,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 3,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 4,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 5,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 6,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 7,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 8,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 9,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+  {
+    id: 10,
+    blogimg: `${BlogImg}`,
+    title: "Lottery changed my life- Local Trader.",
+    body: "Hello rock empire Have you got kids or siblings in elementary or secondary school? The most important resource we have is our children since they are our future. Let’s support you in making provisions for our future leaders",
+  },
+];
